845b2d5b7e1a13a513bd652da16f5e5d88b24ae1
ramonsilva186 <ramonsilva186@gmail.com>
2023-12-03 21:24:37 -0300

    teste commit

--- imobiliaria/src/main/java/br/ifpb/imobiliaria/controller/EnviarEmailServlet.java
+++ imobiliaria/src/main/java/br/ifpb/imobiliaria/controller/EnviarEmailServlet.java
@@ -4,4 +4,6 @@ import jakarta.servlet.http.HttpServlet;
 
 public class EnviarEmailServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
+    private static long Serial2 = 1L;
+
 }
